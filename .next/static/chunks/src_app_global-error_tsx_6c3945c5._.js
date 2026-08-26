eb7c2736f27156aec6f275160712dccb8dbe47e0
M. Cahyo Kriswantoro <137291685+mcahyokriswantoro@users.noreply.github.com>
2026-08-26 11:38:08 +0700

    chore: remove build artifacts from git

--- .next/static/chunks/src_app_global-error_tsx_6c3945c5._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_global-error_tsx_8e7553f5._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_91e4631d.css",
-  "static/chunks/src_components_layout_2654fa0c._.js",
-  "static/chunks/node_modules_bb8ae85e._.js"
-],
-    source: "dynamic"
-});
